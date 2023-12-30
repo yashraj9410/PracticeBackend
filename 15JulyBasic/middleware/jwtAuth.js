037532b991b0966583bf38412045e6e0e2bb539b
Yash Raj <yashraj941015@gmail.com>
2023-12-30 22:01:12 +0530

    updated auth

--- 15JulyBasic/middleware/jwtAuth.js
+++ 15JulyBasic/middleware/jwtAuth.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken')
 const express = require('express')
 
 const verifytoken = (req, res) => {
-    const token = req.headers['authorization'];
+    const token = req.headers['authorization'];  // headeers auth
     if(token){
         const userToken = token.split(' ')[1];
         const decodedToken = jwt.verify(userToken, 'secret1234');
